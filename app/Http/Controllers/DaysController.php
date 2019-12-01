dcf37c1bd382cddbaf2c04d6836b7bdf621e2da7
Alexandre Wavelet <wavelet.alexandre@gmail.com>
2019-12-01 22:35:45 +0100

    Part 2

--- app/Http/Controllers/DaysController.php
+++ app/Http/Controllers/DaysController.php
@@ -10,13 +10,19 @@ class DaysController extends BaseController
 {
     public function one()
     {
-        $fuel_total = 0;
+        $fuel_modules = $fuel_total = 0;
         $inputs = Storage::disk('inputs')->get('day_one.txt');
 
         foreach (explode("\r\n", $inputs) as $input) {
-            $fuel_total += (new Module((int) $input))->getModuleFuelRequirement();
+            $module = new Module((int) $input);
+
+            $fuel_modules += $module->getModuleFuelRequirement();
+            $fuel_total += $module->getFullFuelRequirement();
         }
 
-        return view('days.one', ['fuel' => $fuel_total, 'full_fuel' => 0]);
+        return view('days.one', [
+            'fuel_modules' => $fuel_modules,
+            'fuel_total' => $fuel_total
+        ]);
     }
 }
--- resources/views/days/one.blade.php
+++ resources/views/days/one.blade.php
@@ -3,6 +3,6 @@
 @section('title', 'Day 1')
 
 @section('day')
-    <p>Amount of fuel for modules only: <span class="bold">{{ $fuel }}</span></p>
-    <p>Full amount of fuel: <span class="bold">{{ $full_fuel }}</span></p>
+    <p>Amount of fuel for modules only: <span class="bold">{{ $fuel_modules }}</span></p>
+    <p>Full amount of fuel: <span class="bold">{{ $fuel_total }}</span></p>
 @endsection
