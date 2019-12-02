b4bca3ea3c293457740500beb5c76498b5f9f791
Alexandre Wavelet <wavelet.alexandre@gmail.com>
2019-12-02 21:50:30 +0100

    Unit test P1

--- app/Models/Intcode.php
+++ app/Models/Intcode.php
@@ -6,11 +6,50 @@ use Illuminate\Support\Collection;
 
 class Intcode
 {
-    /** @var Collection */
-    private $integers;
+    private const INSTRUCTION_SIZE = 4;
 
-    function __construct(array $integers)
+    private const OPCODE_ADDITION = 1;
+    private const OPCODE_MULTIPLICATION = 2;
+    private const OPCODE_STOP = 99;
+
+    public function process(array $program): Collection
     {
-        $this->integers = collect($integers);
+        $program = collect($program);
+
+        for ($i=0; $i < ceil($program->count() / self::INSTRUCTION_SIZE); $i++) {
+            $instruction = $program->slice(
+                $i * self::INSTRUCTION_SIZE,
+                self::INSTRUCTION_SIZE
+            )->values();
+
+            $opcode = $instruction->first();
+
+            if ((int) $opcode === self::OPCODE_STOP) {
+                break;
+            }
+
+            list(, $input1, $input2, $output) = $instruction;
+
+            switch ($opcode) {
+                case self::OPCODE_ADDITION:
+                    $program->put(
+                        $output,
+                        $program->get($input1) + $program->get($input2)
+                    );
+                    break;
+
+                case self::OPCODE_MULTIPLICATION:
+                    $program->put(
+                        $output,
+                        $program->get($input1) * $program->get($input2)
+                    );
+                    break;
+
+                default:
+                    throw new Exception('Non-standard OPCode');
+            }
+        }
+
+        return $program;
     }
 }
--- tests/Unit/DayTwoTest.php
+++ tests/Unit/DayTwoTest.php
@@ -9,29 +9,23 @@ class DayTwoTest extends TestCase
 {
     public function testIntcodeProcessCorrectly()
     {
+        $intcode = new Intcode;
+
         $this->assertEquals(
             '2,0,0,0,99',
-            (new Intcode(
-                explode(',', '1,0,0,0,99')
-            ))->process()->implode(',')
+            $intcode->process(explode(',', '1,0,0,0,99'))->implode(',')
         );
         $this->assertEquals(
             '2,3,0,6,99',
-            (new Intcode(
-                explode(',', '2,3,0,3,99')
-            ))->process()->implode(',')
+            $intcode->process(explode(',', '2,3,0,3,99'))->implode(',')
         );
         $this->assertEquals(
             '2,4,4,5,99,9801',
-            (new Intcode(
-                explode(',', '2,4,4,5,99,0')
-            ))->process()->implode(',')
+            $intcode->process(explode(',', '2,4,4,5,99,0'))->implode(',')
         );
         $this->assertEquals(
             '30,1,1,4,2,5,6,0,99',
-            (new Intcode(
-                explode(',', '1,1,1,4,99,5,6,0,99')
-            ))->process()->implode(',')
+            $intcode->process(explode(',', '1,1,1,4,99,5,6,0,99'))->implode(',')
         );
     }
 }
